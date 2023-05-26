7915065c1fc002a7049f435173ccda0f34d6ce02
dhthytran <dhthytran@gmail.com>
2023-05-26 17:31:45 +0700

    s

--- src/main/java/com/social/socialnetwork/repository/UserMessageRepository.java
+++ src/main/java/com/social/socialnetwork/repository/UserMessageRepository.java
@@ -2,11 +2,13 @@ package com.social.socialnetwork.repository;
 
 import com.social.socialnetwork.model.UserMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserMessageRepository extends JpaRepository<UserMessage,Long> {
+    @Query("select uc from UserMessage uc where uc.userId = :id")
     List<UserMessage> findAllByUserId(Long id);
 }
